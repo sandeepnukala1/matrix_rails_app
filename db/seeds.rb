88e91abbfb15dd97d8016a8cf2ac6e3abf1448b3
Sandeep <sandeepn@bu.edu>
2021-06-23 11:25:20 -0400

    Commit 8: Successfully seeded database

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Character.create([
-	{ name: "Neo", description: false },
-	{ name: "Trinity", description: false },
-	{ name: "Morpheus", description: false },
-	{ name: "Agent Smith", description: false },
+	{ name: "Neo", description: "Also known as The One", img_url: "https://en.wikipedia.org/wiki/Neo_(The_Matrix)#/media/File:NeoTheMatrix.jpg" },
+	{ name: "Trinity", description: "Computer programmer and hacker who escaped from the Matrix", img_url: "https://en.wikipedia.org/wiki/Trinity_(The_Matrix)#/media/File:MatrixTrinity.jpg" },
+	{ name: "Morpheus", description: "Captain of the Nedbuchadnezzer" , img_url: "https://en.wikipedia.org/wiki/Morpheus_(The_Matrix)#/media/File:Morpheus.jpg"},
+	{ name: "Agent Smith", description: "AI program in the Matrix to keep order", img_url: "https://en.wikipedia.org/wiki/Agent_Smith#/media/File:Agent_Smith_(The_Matrix_series_character).jpg" },
 ])
